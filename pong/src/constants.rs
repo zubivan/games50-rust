9f6e55495bea0573ec830059e72279c2f92b196f
Ivan Zub <ivan.zub@mendix.com>
2020-05-10 17:41:21 +0200

    Make small font actually smaller

--- pong/src/constants.rs
+++ pong/src/constants.rs
@@ -4,5 +4,5 @@ pub const WORLD_HEIGHT: f32 = 720.;
 pub const VIRTUAL_WORLD_WIDTH: f32 = 432.;
 pub const VIRTUAL_WORLD_HEIGHT: f32 = 243.;
 
-pub const SMALL_FONT_SIZE: f32 = 40.;
+pub const SMALL_FONT_SIZE: f32 = 36.;
 pub const FONT_SIZE: f32 = 40.;
